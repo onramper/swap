59ca8efc6038e0ce38c41e654f166718d6e8959d
Dananjana Kuruppuachchi <dananjana@onramper.com>
2022-09-06 13:23:06 +0530

    updated tf file

--- terraform/swap.tf
+++ terraform/swap.tf
@@ -2,12 +2,7 @@ provider "aws" {
    region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "swap" {
-    bucket = "${var.bucket_name}" 
-    acl = "${var.acl_value}"   
-}
-
-resource "aws_s3_bucket" "swap" {
+resource "aws_s3_bucket" "swap_dev" {
   bucket = "swap_dev"
   acl    = public
 }
