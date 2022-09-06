97a083d8606a315355073a7a54ec65452a1dfed0
Dananjana Kuruppuachchi <dananjana@onramper.com>
2022-09-06 13:27:27 +0530

    update

--- terraform/swap.tf
+++ terraform/swap.tf
@@ -4,5 +4,12 @@ provider "aws" {
 
 resource "aws_s3_bucket" "swap_dev" {
   bucket = "swap_dev"
-  acl    = public
+}
+
+resource "aws_s3_bucket_public_access_block" "swap_dev_acl" {
+  bucket = aws_s3_bucket.swap_dev.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
