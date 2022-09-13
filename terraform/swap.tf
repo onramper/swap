b6374a26293ffd72e324e04307a144c65051792f
Dananjana Kuruppuachchi <dananjana@onramper.com>
2022-09-13 13:47:58 +0530

    update

--- terraform/swap.tf
+++ terraform/swap.tf
@@ -4,16 +4,13 @@ provider "aws" {
 
 resource "aws_s3_bucket" "onramper-swap-dev" {
   bucket = "onramper-swap-dev"
-}
-
-resource "aws_s3_bucket_public_access_block" "swap-dev-acl" {
-  bucket = aws_s3_bucket.onramper-swap-dev.id
   acl    = "public-read" 
   tags = {
     Name        = "swap"
     Environment = "Dev"
   }
 }
+
 
 resource "aws_s3_bucket_object" "object" {
   for_each = fileset("iframe/build/", "*")
