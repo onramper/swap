c961c68ddd2db3df93c58c4c918d4ac22cae6579
Dananjana Kuruppuachchi <dananjana@onramper.com>
2022-09-13 14:28:34 +0530

    update

--- terraform/swap.tf
+++ terraform/swap.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "onramper-swap-dev" {
 
 resource "aws_s3_bucket_object" "object" {
   for_each = fileset("${path.module}/iframe/build/", "**")
-  bucket = aws_s3_bucket.onramper-swap-dev.id
+  bucket = "onramper-swap-dev"
   key    = "each.value"
   source = "${path.module}/iframe/build/${each.value}"
   etag = filemd5("${path.module}/iframe/build/${each.value}")
