599aee459fb20ca177451655993eaf58d2a8a966
Dananjana Kuruppuachchi <dananjana@onramper.com>
2022-09-23 13:32:50 +0530

    update

--- terraform_dev/swap.tf
+++ terraform_dev/swap.tf
@@ -5,7 +5,7 @@ provider "aws" {
 variable "website_root" {
   type        = string
   description = "Path to the root of website content"
-  default     = "${path.module}/../iframe/build"
+  default     = "../iframe/build"
 }
 
 locals {
